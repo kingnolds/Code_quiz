1dc66edaa28166eba517d5ad54bba38fbc1f0f53
Spencer Nelson <srn314@gmail.com>
2022-01-05 21:24:30 -0800

    added comments and removed console logs and pseudo code and uneccissary code

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-// TODO: Make a timer
-//          starts when start quiz button is pressed
-//          subtracts time when a wrong answer is pressed
-//          when the timer hits 0 the game ends
 
-// TODO: Make the quiz
-//          Question text changes when game starts and after questions are entered
-//          buttons appear with the answer options
-//          when answer is clicked, changes to next question and displays either correct or incorrect
-//          if incorrect timer loses time
-//          once all questions are answered input name or initials and submit
-//          open highscore page
-//              EXTRA: randomize quiz questions and answers
+// Selectors for HTML elements:
+const quizBase = document.querySelector("#welcome");
+const startButton = document.querySelector("#quiz-button");
+const quizGame = document.querySelector("#quiz");
+const quizQuestion = document.querySelector("#question");
+const quizEnd = document.querySelector("#quiz-end");
+const highScores = document.querySelector("#score-page");
+const playAgain = document.getElementsByClassName("play-again");
+const scoreList = document.querySelector("#score-list");
+const quizAnswers = document.getElementsByClassName("answer");
+const rightWrong = document.querySelector("#answer-result");
+const viewHighscores = document.querySelector('.highscore-redirect');
+const header = document.querySelector("#header");
+const timerDisplay = document.querySelector("#time-left"); 
 
-// TODO: Make Highscore page
-//          on end of quiz and submit highscore or click view highscore show screen with all highscores and a clear button
-//          on clear button press remove all highscores
+// Quiz variables
+let quizIndex = 0
+let shuffledQuiz = []
+let shuffledAnswers = []
+
+
 
 
 // Timer
-const timerDisplay = document.querySelector("#time-left"); 
+
 let timeLeft = 60
 let timeInterval
 
@@ -31,19 +36,19 @@ function timer() {
       timeLeft--; // countdown by 1
       timerDisplay.textContent = timeLeft; // display remaining time
   
-      if(timeLeft <= 0) { // when timer hits zero
+      if(timeLeft <= 0) { // when timer hits zero or below in case of wrong answer
         timeLeft = 0
         timerDisplay.textContent = timeLeft;
         clearInterval(timeInterval); // stop the timer
         
-        gameOver(); 
+        gameOver(); // go to end game screen
       }
   
     }, 1000);
   }
   
-// all questions available 
 
+// Question Bank
 const questionList = [
     {
     question: "Which of these are the names of Joe's cats?",
@@ -82,25 +87,7 @@ const questionList = [
     },
 ]
 
-  // Quiz start
-const quizBase = document.querySelector("#welcome");
-const startButton = document.querySelector("#quiz-button");
-const quizGame = document.querySelector("#quiz");
-const quizQuestion = document.querySelector("#question");
-const quizEnd = document.querySelector("#quiz-end");
-const highScores = document.querySelector("#score-page");
-const playAgain = document.getElementsByClassName("play-again");
-const scoreList = document.querySelector("#score-list")
-var quizAnswers = document.getElementsByClassName("answer");
-let rightWrong = document.querySelector("#answer-result");
-const viewHighscores = document.querySelector('.highscore-redirect');
-const header = document.querySelector("#header")
-let quizIndex = 0
-let shuffledQuiz = []
-let shuffledAnswers = []
-
-
-console.log(playAgain)
+// Quiz Functions
 function showQuestion() {
     
     quizQuestion.textContent = shuffledQuiz[quizIndex].question; // load question into the h1
@@ -110,13 +97,13 @@ function showQuestion() {
     
   }
 
-function newQuestion() {
+function newQuestion() { // if there are questions left, go to the next question
     if (quizIndex < (shuffledQuiz.length-1)) {
         quizIndex++; 
         showQuestion();
-    } else {
+    } else { // if there are no questions left end the quiz
         clearInterval(timeInterval)
-        setTimeout(function() {
+        setTimeout(function() { // stay on quiz to show if the last answer was right or wrong
             gameOver()
         }, 500)
     }
@@ -129,62 +116,54 @@ function quizStart() {
     header.setAttribute("style", "visibility: visible;"); // show header if coming from highscore page
     quizGame.setAttribute("style", "display: flex;"); // display quiz
 
-    shuffledQuiz = questionList.sort((a, b) => 0.5 - Math.random());
-    for (let i = 0; i < questionList.length; i++) {
+    shuffledQuiz = questionList.sort((a, b) => 0.5 - Math.random()); // shuffle the questions
+    for (let i = 0; i < questionList.length; i++) { // shuffle the answers for each question
         shuffledAnswers = questionList[i].answers.sort((a, b) => 0.5 - Math.random());
         shuffledQuiz.answers = shuffledAnswers
     }
-    console.log(shuffledAnswers)
-    console.log(questionList)
     
-    quizIndex = 0
-    showQuestion();
-    // quizBase.setAttribute("style", "display: block;"); //display quiz page
+    quizIndex = 0 // set question index to 0
+    showQuestion(); // display a question
     timer(); //start time
 }
 
 let showRightWrong
-console.log(questionList)
-function readAnswer(answer) {
-    console.log(answer)
-    console.log(questionList)
-    console.log(questionList[quizIndex].answers[0])
-    if (questionList[quizIndex].correct === answer) {
+
+function readAnswer(answer) { // for the selected answer determine if it is correct or not, the answer is read from the event listener on the answer buttons
+    if (questionList[quizIndex].correct === answer) { // if the answer button clicked has the same value as the question's correct value
         rightWrong.textContent = "Correct!"
-        rightWrong.classList.add("correct");
-        rightWrong.setAttribute("style", "visibility: visible;")
+        rightWrong.classList.add("correct"); // add text with class correct 
         clearTimeout(showRightWrong);
-        showRightWrong = setTimeout(function () {
+        showRightWrong = setTimeout(function () { // remove the text and class after 1 second
             rightWrong.textContent = ""
             rightWrong.classList.remove("correct");
         }, 1000);
-    } else {
-        timeLeft -= 15;
+    } else { // if incorrect
+        timeLeft -= 15; // lose 15 seconds
         rightWrong.textContent = "Wrong."
-        rightWrong.classList.add("wrong");
-        rightWrong.setAttribute("style", "visibility: visible;")
+        rightWrong.classList.add("wrong"); // add text with red background
         clearTimeout(showRightWrong);
-        showRightWrong = setTimeout(function () {
+        showRightWrong = setTimeout(function () { // remove after 1 second
             rightWrong.textContent = ""
             rightWrong.classList.remove("wrong");
         }, 1000);
     }
-    newQuestion();
+    newQuestion(); // show next question
 }
 
-startButton.addEventListener("click", quizStart); // start when start button is pressed  \
-for (let i = 0; i < playAgain.length; i++) {
+startButton.addEventListener("click", quizStart); // when start button is clicked start quiz
+for (let i = 0; i < playAgain.length; i++) { // when any play again button is pressed 
     playAgain[i].addEventListener("click", function () {
-        quizEnd.setAttribute("style", "display: none;");
+        quizEnd.setAttribute("style", "display: none;"); // hide all but the welcome screen
         quizBase.setAttribute("style", "display: flex;");
         quizGame.setAttribute("style", "display: none;");
         highScores.setAttribute("style", "display: none;");
-        header.setAttribute("style", "visibility: visible;")
+        header.setAttribute("style", "visibility: visible;"); // show the header if coming from highscore screen
     })
     }
     
 
-document.querySelectorAll(".answer").forEach(element => {
+document.querySelectorAll(".answer").forEach(element => { // when any one of the answers are clicked send the text content of that answer button to be checked in the read answer function 
     element.addEventListener("click", event =>{
     const clicked = event.target;
     if (clicked.matches("button")) {
@@ -194,75 +173,64 @@ document.querySelectorAll(".answer").forEach(element => {
     
 });
 
-// gameover: stop timer at 0 or set to 0 if lower
-//      hide quiz screen, show end game screen
-//      display too bad try again message if score is 0
-//      display you win and enter your initials if score is > 0
-//      once you enter score send to highscore screen
+// End Game screen
 const quizResult = document.querySelector("#quiz-result");
 const endPrompt = document.querySelector("#result-prompt");
 const scoreInput = document.querySelector("#score");
 
-function gameOver() {
-    clearInterval(timeInterval)
-    quizGame.setAttribute("style", "display: none;")
-    quizEnd.setAttribute("style", "display: flex;")
-    if (timeLeft <= 0) {
-        timeLeft = 0;
+function gameOver() { 
+    clearInterval(timeInterval) // stop timer
+    quizGame.setAttribute("style", "display: none;") // hide quiz screen
+    quizEnd.setAttribute("style", "display: flex;") // show end game screen
+    if (timeLeft <= 0) { // if timer is 0 or below
+        timeLeft = 0; // set to 0
         quizResult.textContent = "Game Over!"
         endPrompt.textContent = "You must answer all questions and get at least one correct. Better luck next time!"
-        scoreInput.setAttribute("style", "display: none;");
-        rightWrong.classList.remove("correct");
+        scoreInput.setAttribute("style", "display: none;"); // hide score input
+        rightWrong.classList.remove("correct"); // remove right wrong case in edge case
         rightWrong.classList.remove("wrong");
     } else {
         quizResult.textContent = "You Win!"
         endPrompt.textContent = "Save your Highscore!"
-        scoreInput.setAttribute("style", "display: block;");
+        scoreInput.setAttribute("style", "display: block;"); // show score input
     }
 
 }
 
-function toHighscorePage() {
+function toHighscorePage() { // hide all but highscore page
     quizEnd.setAttribute("style", "display: none;");
     quizBase.setAttribute("style", "display: none;");
     quizGame.setAttribute("style", "display: none;");
     highScores.setAttribute("style", "display: block;");
-    header.setAttribute("style", "visibility: hidden;")
+    header.setAttribute("style", "visibility: hidden;"); // hide header with timer and view high score prompt
 }
-console.log(header)
+
+// Highscore page 
 const submitScoreBtn = document.querySelector("#submit-score")
 const initialsInput = document.querySelector("#score-initials")
 const clearScores = document.querySelector("#clear-scores")
 
-
-// highscore page
-//    hide game over screen show highscore page
-//    input and sort high scores 
-//    clear highscores button and play again button
-//    highscores set to local storage
-
 let scores = [];
 
-function sortScores(a, b) {
+function sortScores(a, b) { // sort scores descending by time
    return b.time - a.time;
 }
 
-function renderScores() {
-    scoreList.innerHTML = "";
+function renderScores() { // create score list
+    scoreList.innerHTML = ""; // clear scores
 
     scores.sort(sortScores);
 
-    for (let i = 0; i < scores.length; i++) {
+    for (let i = 0; i < scores.length; i++) { // add list element for every score with rank initials and time
         let score = scores[i];
         let scoreLi = document.createElement("li");
         scoreLi.textContent = `${i+1}. ${score.initials} ${score.time}`;
-        console.log(score)
-        scoreList.appendChild(scoreLi)
+        scoreList.appendChild(scoreLi) 
     }
 }
 
 function init() {
-    let storedScores = JSON.parse(localStorage.getItem("scores"));
+    let storedScores = JSON.parse(localStorage.getItem("scores")); // pull any stored scores from local storage
 
     if (storedScores !== null) {
         scores = storedScores;
@@ -270,32 +238,32 @@ function init() {
     renderScores();
 }
 
-function storeScores() {
+function storeScores() { // store scores in local storage
     localStorage.setItem("scores", JSON.stringify(scores));
 }
 
 
-submitScoreBtn.addEventListener("click", function (event) {
+submitScoreBtn.addEventListener("click", function (event) { // when scores are submitted
     event.preventDefault();
     let playerScore = {
         initials: initialsInput.value.trim(),
         time: timeLeft
     }
-    if(playerScore.initials === "") {
+    if(playerScore.initials === "") { // if nothing in initials stop function
         return;
     }
-    scores.push(playerScore);
+    scores.push(playerScore); // add the submitted score to the scores list
     storeScores();
     renderScores();
-    toHighscorePage();
+    toHighscorePage(); // send to highscore page
 })
 
-clearScores.addEventListener("click", function () {
+clearScores.addEventListener("click", function () { // clear scores function empties all recorded scores in local storage
     scores = []
     localStorage.removeItem("scores")
     renderScores();
 })
 
-viewHighscores.addEventListener("click",toHighscorePage);
+viewHighscores.addEventListener("click",toHighscorePage); // when view highscores is pressed send to highscore screen
 
-init();
+init(); // pull local storage and create score list on opening page
